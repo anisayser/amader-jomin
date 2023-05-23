0f1b8bde5d12050859f64509ae4c6eddaeee766d
Anis Ayser <anisayser1@gmail.com>
2023-05-23 23:43:54 +0600

    Project Demo

---
+++ src/assets/main.js
@@ -0,0 +1,31 @@
+// var moment = require("moment");
+// import moment from "moment";
+
+const menuItems = document.querySelector('.menuItems');
+const searchInput = document.querySelector('.searchInput');
+const openSearch = document.getElementsByClassName('openSearch');
+const closeSearch = document.getElementById('closeSearch');
+const theDateField = document.querySelector('.theDate');
+theDateField.innerHTML = moment().locale('bn').format('LLLL');
+
+console.log(openSearch)
+
+for (const searchOpen of openSearch) {
+    searchOpen.addEventListener('click', ()=>{
+        menuItems.classList.remove('d-flex');
+        menuItems.classList.add('d-none');
+        searchInput.classList.remove('d-none');
+        searchInput.classList.add('d-flex');
+    })
+}
+
+// Closing search input and showing menu items
+closeSearch.addEventListener('click', ()=>{
+    menuItems.classList.remove('d-none');
+    menuItems.classList.add('d-flex');
+    searchInput.classList.remove('d-flex');
+    searchInput.classList.add('d-none');
+})
+
+
+
