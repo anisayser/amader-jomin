7ed61e303682b2d369a31b89f323ca7ab39efc2b
Anis Ayser <anisayser1@gmail.com>
2023-05-25 14:57:19 +0600

    Menu Issue Solved

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,9 +2,13 @@
 // import moment from "moment";
 
 const menuItems = document.querySelector('.menuItems');
+const headerItems = document.querySelector('.headerItems');
 const searchInput = document.querySelector('.searchInput');
 const openSearch = document.getElementsByClassName('openSearch');
 const closeSearch = document.getElementById('closeSearch');
+const searchInputOne = document.querySelector('.searchInputOne');
+const openSearchOne = document.querySelector('.openSearchOne');
+const closeSearchOne = document.getElementById('closeSearchOne');
 const theDateField = document.querySelector('.theDate');
 
 const d = new Date().toLocaleDateString('bn');
@@ -13,6 +17,21 @@ theDateField.innerHTML = d;
 
 console.log(openSearch)
 
+// Mobile Search Controllers
+    openSearchOne.addEventListener('click', ()=>{
+        headerItems.classList.add('d-none');
+        searchInputOne.classList.remove('d-none');
+        searchInputOne.classList.add('d-flex');
+    })
+// Closing search input and showing menu items
+closeSearchOne.addEventListener('click', ()=>{
+    headerItems.classList.remove('d-none');
+    searchInputOne.classList.remove('d-flex');
+    searchInputOne.classList.add('d-none');
+})
+
+
+// Desktop Search Controllers
 for (const searchOpen of openSearch) {
     searchOpen.addEventListener('click', ()=>{
         menuItems.classList.remove('d-flex');
